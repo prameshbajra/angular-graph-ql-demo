502dac5d6d63b20b26644c2b5441449506347bd6
prameshbajra <pe.messh@gmail.com>
2019-10-11 21:34:54 +0545

    Space X API under process ...
    angular material in play.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,25 @@ import { LaunchListComponent } from './launch-list/launch-list.component';
 import { LaunchDetailsComponent } from './launch-details/launch-details.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LaunchListComponent,
-    LaunchDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    GraphQLModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LaunchListComponent,
+        LaunchDetailsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        GraphQLModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/launch-list/launch-list.component.ts
+++ src/app/launch-list/launch-list.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { PastLaunchesListGQL } from '../services/spacexGraphql.service';
 
 @Component({
-  selector: 'app-launch-list',
-  templateUrl: './launch-list.component.html',
-  styleUrls: ['./launch-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-launch-list',
+    templateUrl: './launch-list.component.html',
+    styleUrls: ['./launch-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchListComponent implements OnInit {
 
-  constructor() { }
+    constructor(private pastLaunchesServices: PastLaunchesListGQL) { }
 
-  ngOnInit() {
-  }
+    pastLaunches$ = this.pastLaunchesServices.fetch({ limit: 20 })
+        .pipe(
+            map(res => res.data.launchesPast)
+        );
+
+    ngOnInit() {
+    }
 
 }
